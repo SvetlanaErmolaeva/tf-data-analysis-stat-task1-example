06b484ad3a733085775bc1a9b2e22a5a4f047d2d
SvetlanaErmolaeva <61152365+SvetlanaErmolaeva@users.noreply.github.com>
2023-04-12 13:16:36 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,4 @@ import numpy as np
 chat_id = 411069874 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t = 10
-    n = len(x)
-    mu = -29
-    sigma = np.exp(1)
-    errors = np.random.normal(mu, sigma, n)
-    a = errors/t
-    return a.mean()
+    return (x.mean() + 28) / 10
